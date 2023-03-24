84baf969a818dc6f7918daa4214581998dce1eb7
Abdul Haseeb Khan <haseeb29jan@gmail.com>
2023-03-24 11:42:03 +0400

    Deploy to pages

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+Ahk294/cst3145-individual-cw3
+git push -f git@github.com:Ahk294/cst3145-individual-cw3.git master:gh-pages
+cd -
